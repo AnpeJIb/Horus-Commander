7eb2a93cc4be7b5b21d8e0c4c43258f03e68d405
Alexander Oblovatniy <oblovatniy@gmail.com>
2012-11-30 01:54:50 +0200

    l10n update

--- Commander/l10n/ru.ts
+++ Commander/l10n/ru.ts
@@ -8,28 +8,98 @@
         <source>Horus Commander</source>
         <translation>Хорус Коммандер</translation>
     </message>
+    <message>
+        <location filename="../mainwindow.ui" line="36"/>
+        <source>Main</source>
+        <translation>Главная</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="45"/>
+        <source>Log</source>
+        <translation>Журнал</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="63"/>
+        <source>&amp;Tools</source>
+        <translation>&amp;Инструменты</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="84"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Выход</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="87"/>
+        <source>Ctrl+X</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="96"/>
+        <source>&amp;Minimize</source>
+        <translation>&amp;Свернуть</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="99"/>
+        <source>Alt+M</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="108"/>
+        <source>&amp;Show</source>
+        <translation>&amp;Показать</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="111"/>
+        <source>Alt+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="107"/>
+        <source>Closing window</source>
+        <translation>Закрытие окна</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="108"/>
+        <source>Do you wish to quit? Otherwise window will be minimized to tray</source>
+        <translation>Вы хотите выйти? В противном случае окно будет свернуто в трей</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="109"/>
+        <source>Do not prompt again</source>
+        <translation>Больше не спрашивать</translation>
+    </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../config/config.cpp" line="27"/>
+        <location filename="../config/config.cpp" line="31"/>
         <source>Could not open config file &quot;%1&quot; for writing</source>
         <translation>Невозможно открыть файл настроек &quot;%1&quot; для записи</translation>
     </message>
     <message>
-        <location filename="../config/config.cpp" line="58"/>
+        <location filename="../config/config.cpp" line="62"/>
         <source>Could not open config file &quot;%1&quot; for reading</source>
         <translation>Невозможно открыть файл настроек &quot;%1&quot; для чтения</translation>
     </message>
     <message>
-        <location filename="../config/config.cpp" line="69"/>
+        <location filename="../config/config.cpp" line="73"/>
         <source>Bad config file</source>
         <translation>Некоректный файл настроек</translation>
     </message>
     <message>
-        <location filename="../config/config.cpp" line="82"/>
+        <location filename="../config/config.cpp" line="86"/>
         <source>Wrong config root</source>
         <translation>Некоректный корневой элемент настроек</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="51"/>
+        <source>Systray</source>
+        <translation>Системный трей</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="52"/>
+        <source>I couldn&apos;t detect any system tray on this system.</source>
+        <translation>Невозможно определить системный трей в данной системе.</translation>
     </message>
 </context>
 <context>
