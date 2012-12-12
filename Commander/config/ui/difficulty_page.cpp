bd8999e16820f7970111ec8998c3761ebceddc4e
AnpeJIb <anpejib@gmail.com>
2012-12-12 21:02:44 +0400

    difficulty
    difficulty

---
+++ Commander/config/ui/difficulty_page.cpp
@@ -0,0 +1,117 @@
+#include "difficulty_page.h"
+#include "ui_difficulty_page.h"
+#include "difficulty_flight_model_page.h"
+#include "difficulty_weapons_page.h"
+#include "difficulty_view_page.h"
+#include "difficulty_map_icons_page.h"
+#include "difficulty_var_page.h"
+
+DifficultyPage::DifficultyPage(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::DifficultyPage)
+{
+    ui->setupUi(this);
+    setSplitterPos();
+    addPages();
+    load();
+}
+
+DifficultyPage::~DifficultyPage()
+{
+    delete ui;
+}
+
+bool DifficultyPage::save()
+{
+    return saveChildren();
+}
+
+void DifficultyPage::load()
+{
+    return loadChildren();
+}
+
+void DifficultyPage::loadDefaults()
+{
+    return loadChildrenDefaults();
+}
+
+QString DifficultyPage::pageName()
+{
+    return ui->pageName->text();
+}
+
+void DifficultyPage::setSplitterPos()
+{
+    QList<int> sizes;
+    int listW = qMin((width()*20)/100, 140);
+    sizes << listW;
+    sizes << width()-listW;
+    ui->splitter->setSizes(sizes);
+}
+
+void DifficultyPage::addPages()
+{
+    DifficultyFlightModelPage* fm = new DifficultyFlightModelPage;
+    new QListWidgetItem(
+                tr("Flight model"),
+                ui->list,
+                QListWidgetItem::UserType);
+    ui->stack->addWidget(fm);
+    subpages << fm;
+
+    DifficultyWeaponsPage* wpn = new DifficultyWeaponsPage;
+        new QListWidgetItem(
+                    tr("Weapons"),
+                    ui->list,
+                    QListWidgetItem::UserType);
+        ui->stack->addWidget(wpn);
+        subpages << wpn;
+
+        DifficultyViewPage* vw = new DifficultyViewPage;
+        new QListWidgetItem(
+                    tr("View"),
+                    ui->list,
+                    QListWidgetItem::UserType);
+        ui->stack->addWidget(vw);
+        subpages << vw;
+
+        DifficultyMapIconsPage* mi = new DifficultyMapIconsPage;
+        new QListWidgetItem(
+                    tr("Map & icons"),
+                    ui->list,
+                    QListWidgetItem::UserType);
+        ui->stack->addWidget(mi);
+        subpages << mi;
+
+        DifficultyVarPage* var = new DifficultyVarPage;
+        new QListWidgetItem(
+                    tr("Various"),
+                    ui->list,
+                    QListWidgetItem::UserType);
+        ui->stack->addWidget(var);
+        subpages << var;
+
+        ui->stack->setCurrentIndex(0);
+        ui->list->setCurrentRow(0);
+}
+
+void DifficultyPage::on_easyButton_clicked()
+{
+
+}
+
+void DifficultyPage::on_normalButton_clicked()
+{
+
+}
+
+void DifficultyPage::on_realButton_clicked()
+{
+
+}
+
+void DifficultyPage::on_listWidget_currentItemChanged(QListWidgetItem *current, QListWidgetItem *previous)
+{
+
+}
---
+++ Commander/config/ui/difficulty_page.h
@@ -0,0 +1,46 @@
+#ifndef DIFFICULTY_PAGE_H
+#define DIFFICULTY_PAGE_H
+
+#include <QWidget>
+#include <QList>
+#include <QListWidgetItem>
+#include "uiconfig_module.h"
+#include "named_page.h"
+#include "difficulty_subpage.h"
+
+namespace Ui {
+class DifficultyPage;
+}
+
+class DifficultyPage : public QWidget, public UIConfigModule, public NamedPage
+{
+    Q_OBJECT
+    
+public:
+    explicit DifficultyPage(QWidget *parent = 0);
+    ~DifficultyPage();
+
+    bool save();
+    void load();
+    void loadDefaults();
+
+    QString pageName();
+    
+private slots:
+    void on_easyButton_clicked();
+
+    void on_normalButton_clicked();
+
+    void on_realButton_clicked();
+
+    void on_listWidget_currentItemChanged(QListWidgetItem *current, QListWidgetItem *previous);
+
+private:
+    void setSplitterPos();
+    void addPages();
+
+    Ui::DifficultyPage *ui;
+    QList<DifficultySubpage*> subpages;
+};
+
+#endif // DIFFICULTY_PAGE_H
